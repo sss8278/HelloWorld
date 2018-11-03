042cc2b98cb08d9f7b3ddb8efadba3061bd9e3dc
Samuel Ip <a4g2b@use.cs.ubc.ca>
2018-11-03 12:49:05 -0700

    Initial commit

---
+++ src/com/stephaniesimpson/helloworld/Main.java
@@ -0,0 +1,9 @@
+package com.stephaniesimpson.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+	// write your code here
+    }
+}
